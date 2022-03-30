b24511431dffac4c919ac8f3e54187d24d2828cc
Кондакова Татьяна <rotaru2803@rambler.ru>
2022-03-30 15:28:02 +0500

    add all

---
+++ src/main/java/Main.java
@@ -0,0 +1,116 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+//первая
+        String[] employee1 = "1,John,Smith,USA,25".split(",");
+        String[] employee2 = "2,Inav,Petrov,RU,23".split(",");
+        try (CSVWriter writer = new CSVWriter(new FileWriter("data.csv", true))) {
+            writer.writeNext(employee1);
+            writer.writeNext(employee2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileNameCsv = "data.csv";
+        List<Employee> list = paseCSV(fileNameCsv, columnMapping);
+        String json = listToJson(list);
+        String jsonName = "data.json";
+        writeString(json, jsonName);
+//первая
+
+// вторая
+        String fileNameXml = "data.XML";
+        List<Employee> list1 = parseXML(fileNameXml);
+        String jsonName2 = "data2.json";
+        String json1 = listToJson(list1);
+        writeString(json1, jsonName2);
+    }
+//вторая
+
+    private static List paseCSV(String fileName, String[] columnMapping) {
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csv.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    private static void writeString(String json, String jsonName) {
+        try (FileWriter writer = new FileWriter(jsonName)) {
+            writer.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Employee> parseXML(String xmlFilename) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(xmlFilename));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        List<String> elements = new ArrayList<>();
+        List<Employee> listFromXml = new ArrayList<>();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeName().equals("employee")) {
+                NodeList nodeList1 = node.getChildNodes();
+                for (int j = 0; j < nodeList1.getLength(); j++) {
+                    Node node1 = nodeList1.item(j);
+                    if (Node.ELEMENT_NODE == node1.getNodeType()) {
+                        elements.add(node1.getTextContent());
+                    }
+                }
+                listFromXml.add(new Employee(
+                        Long.parseLong(elements.get(0)),
+                        elements.get(1),
+                        elements.get(2),
+                        elements.get(3),
+                        Integer.parseInt(elements.get(4))));
+                elements.clear();
+            }
+        }
+        return listFromXml;
+    }
+}
